3ce7cfbe7e7fc67824c1397d3de057332325d82f
RaveHunter05 <paulsotelo97@gmail.com>
2020-08-02 00:06:51 -0600

    cambios leves en django

--- src/productos/models.py
+++ src/productos/models.py
@@ -4,8 +4,11 @@ from django.db import models
 
 class Productos(models.Model):
     title = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     photo = models.ImageField(upload_to='imagenes_productos/')
+    
+    #Aqui haria falta escribir el proveedor y la categoría del producto
 
     def __str__(self):
         return self.title
