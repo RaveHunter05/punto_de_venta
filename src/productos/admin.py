70e5ac23107eeada41c74572a33b0d3721213ea6
RaveHunter05 <paulsotelo97@gmail.com>
2020-08-27 22:49:43 -0600

    registrando en pestaña del admin los modelos

--- src/productos/admin.py
+++ src/productos/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import IngresarProductos
+from .models import *
 
+admin.site.register(Suppliers)
+admin.site.register(Categories)
 admin.site.register(IngresarProductos)
+admin.site.register(Employees)
+admin.site.register(Customers)
+admin.site.register(Shippers)
+admin.site.register(Orders)
+admin.site.register(Order_details)
